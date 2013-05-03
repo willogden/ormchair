4d925bd65229027c93b663f4da6c74e3e41fd915
Will Ogden <will.ogden@gmail.com>
2013-05-04 00:10:25 +0100

    Configured setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name='ormchair',
+	version='0.1',
+	long_description=open('README').read(),
+	author='Will Ogden',
+	url='https://github.com/willogden/ormchair',
+	py_modules=['ormchair'],
+	packages=['tests',],
+	install_requires=['requests>=1.2.0']
+)
